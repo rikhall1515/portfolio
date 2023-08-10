24c1e000cde0fbc07c2a36de5a3a417a3d586bf5
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2023-08-10 19:57:56 +0200

    Footer section done.

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -2,11 +2,14 @@ import { component$ } from "@builder.io/qwik";
 import Link from "~/components/link";
 export default component$(() => {
   return (
-    <footer class="py-32" aria-label="About the website">
-      <div class="max-w-[120rem] m-auto px-32">
-        <p class="font-atkinson w-[60ch]">
+    <footer
+      class="py-[--section-y-spacing] bg-gradient-to-b from-secondary_350 to-secondary_400"
+      aria-label="About the website"
+    >
+      <div class="max-w-[120rem] m-auto page-outer-spacing">
+        <p class="font-atkinson max-w-[60ch]">
           This portfolio was meticulously designed with Figma and built using
-          Next.js, and TailwindCSS.{" "}
+          Qwik, Typescript, and TailwindCSS.{" "}
           <Link
             text="Look at the portfolio GitHub"
             href="https://github.com/rikhall1515"
