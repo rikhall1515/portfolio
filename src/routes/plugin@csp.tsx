ec45c037f4c19ec8afc0a69d6c77379fff0e53f6
rikhall1515 <30295873+rikhall1515@users.noreply.github.com>
2023-08-22 09:09:17 +0200

    checking to see if this works better for CSP headers

--- src/routes/plugin@csp.tsx
+++ src/routes/plugin@csp.tsx
@@ -7,17 +7,18 @@ export const onRequest: RequestHandler = (event) => {
   event.sharedMap.set("@nonce", nonce);
   const csp = [
     `upgrade-insecure-requests`,
-    `default-src 'self' 'unsafe-inline'`,
+    `default-src 'self'`,
     `font-src 'self'`,
-    `img-src 'self' 'unsafe-inline' data: https: blob:`,
-    `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' *.cloudflareinsights.com */cdn-cgi/*`,
+    `img-src 'self' data: https: blob:`,
+    `script-src 'self' 'strict-dynamic' 'nonce-${nonce}'`,
+    `connect-src cloudflareinsights.com`,
     `style-src 'self' 'unsafe-inline'`,
     `frame-src 'self' 'nonce-${nonce}'`,
     `object-src 'none'`,
     `base-uri 'self'`,
   ];
 
-  const sts = [`max-age '30'`, `includeSubDomains`, `preload`];
+  const sts = [`max-age 30`, `includeSubDomains`, `preload`];
 
   event.headers.set("Content-Security-Policy", csp.join("; "));
   event.headers.set("Strict-Transport-Security", sts.join("; "));
